d20c688aa310c2f7d71c03efc4b6e6daa090de13
Ryan Parrish <churchofslidin@gmail.com>
2025-01-16 08:56:05 -0700

    added functions docs and fixed typo

--- libs/utils/decorate.js
+++ libs/utils/decorate.js
@@ -25,6 +25,15 @@ export function decorateButtons(el) {
   });
 }
 
+/**
+ * Adjusts the focal point of an image within a picture element
+ * based on the provided child element's content.
+ *
+ * @param {HTMLElement} pic - The picture element containing the image.
+ * @param {HTMLElement} child - The child element containing focal point data.
+ * @param {boolean} removeChild - Flag indicating whether to remove the child element
+ * or its text nodes after processing.
+ */
 export function handleFocalpoint(pic, child, removeChild) {
   const image = pic.querySelector('img');
   if (!child || !image) return;
@@ -43,6 +52,7 @@ export function handleFocalpoint(pic, child, removeChild) {
   const [x, y = ''] = directions;
   image.style.objectPosition = `${x} ${y}`;
 }
+
 
 export async function decorateBlockBg(block, node, { useHandleFocalpoint = false, className = 'background' } = {}) {
   const childCount = node.childElementCount;
