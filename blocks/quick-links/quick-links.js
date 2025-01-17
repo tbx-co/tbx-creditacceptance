67425e8cf1a8dc02b96a846485510685f74b4eb2
tbxsaugat <111308646+tbxsaugat@users.noreply.github.com>
2025-01-17 13:56:16 -0500

    [#16 - Quick Links] (#58)
    * feat: quick links block
    
    * style: add padding on block container
    
    * add color variables
    
    * add icons
    
    * fix svg style
    
    * fix style on hover on icon
    
    ---------
    
    Co-authored-by: Saugat Malla <saugat@thinkinbox.com>
    Co-authored-by: kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>

---
+++ blocks/quick-links/quick-links.js
@@ -0,0 +1,30 @@
+export default function decorate(block) {
+  const nav = document.createElement('nav');
+  const ul = document.createElement('ul');
+  nav.append(ul);
+
+  Array.from(block.children).forEach((row) => {
+    const li = document.createElement('li');
+    const a = row.querySelector('a');
+    a.className = 'quick-links-item';
+    const { textContent } = a;
+    a.textContent = '';
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = textContent;
+    a.append(textSpan);
+
+    const { src: iconSrc } = row.querySelector('.icon img');
+    const maskedDiv = document.createElement('div');
+    maskedDiv.className = 'icon-masked';
+    maskedDiv.style.mask = `url(${iconSrc}) no-repeat center`;
+
+    a.prepend(maskedDiv);
+
+    li.append(a);
+    ul.append(li);
+  });
+
+  block.innerHTML = '';
+  block.append(nav);
+}
