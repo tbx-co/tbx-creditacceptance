4c865df54ed23404497e8d9d2afcf874d681814c
Ryan Parrish <churchofslidin@gmail.com>
2025-02-21 16:06:56 -0700

    #60 - Marquee animated (#260)
    * added animated marquee
    
    * adjusted mobile animation and setInterval to auto toggle every 8s
    
    * no setTimeout needed

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -1,5 +1,6 @@
 import { createTag } from '../../libs/utils/utils.js';
 import { decorateBlockBg, isDarkHexColor } from '../../libs/utils/decorate.js';
+import { loadCSS } from '../../scripts/aem.js';
 
 function isDarkColor(colors, colorStr) {
   const colorObject = colors.find((c) => c['brand-name'] === colorStr);
@@ -37,6 +38,43 @@ function decorateIntro(el) {
   }
 }
 
+function addCoins(el) {
+  for (let i = 0; i < 10; i += 1) {
+    const coinAttrs = {
+      src: `${window.hlx.codeBasePath}/blocks/marquee/animated/big-coin.gif`,
+      class: `coin coin-${i + 1}`,
+    };
+    const coinImg = createTag('img', coinAttrs);
+    el.append(coinImg);
+  }
+}
+
+function initAnimatedMarquee(block) {
+  const headings = block.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  headings.forEach((heading, i) => {
+    heading.classList.add(`view-${i + 1}`);
+  });
+  const foreground = block.querySelector('.foreground');
+  foreground.children[0].classList.add('toggle-copy');
+
+  const input = createTag('input', { type: 'checkbox', id: 'btnToggle', name: 'btnToggle' });
+  const slider = createTag('span', { class: 'slider' });
+  const label = createTag('label', { class: 'toggle' }, [input, slider]);
+
+  const toggleAria = createTag('div', { class: 'toggle-aria' }, label);
+  foreground.append(toggleAria);
+  addCoins(foreground);
+
+  const toggleClass = () => { block.classList.toggle('toggled', input.checked); };
+  input.addEventListener('change', toggleClass);
+
+  // Auto-toggle every 8 seconds
+  setInterval(() => {
+    input.checked = !input.checked;
+    toggleClass();
+  }, '8000');
+}
+
 export default function decorate(block) {
   const children = block.querySelectorAll(':scope > div');
   const foreground = children[children.length - 1];
@@ -55,4 +93,8 @@ export default function decorate(block) {
   const lastAction = buttons[buttons.length - 1]?.closest('p, div');
   if (!lastAction) return;
   lastAction.nextElementSibling?.classList.add('supplemental-text');
+  if (block.classList.contains('animated-toggle')) {
+    loadCSS(`${window.hlx.codeBasePath}/blocks/marquee/animated/animated-toggle.css`);
+    initAnimatedMarquee(block);
+  }
 }
