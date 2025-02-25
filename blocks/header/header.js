7e146be82d62aac96e1cdfb1399b09c16d2f9c8a
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2025-02-25 11:02:56 -0500

    Kailas pregolive (#296)
    * Create helix-query.yaml
    
    * Buttons, fragment autoblocking, Icon CTAs
    
    * devider
    
    * divider
    
    * Separator based section grids
    
    * Update cards.css
    
    * Update cards.css
    
    * icons
    
    * Update columns.css
    
    * change
    
    * Update styles.css
    
    * icons
    
    * review comments
    
    * fix
    
    * Update scripts.js
    
    * divider-2
    
    * Update decorate.js
    
    * Update scripts.js
    
    * Update styles.css
    
    * comment
    
    * contact us
    
    * Update columns.css
    
    * Update columns.css
    
    * Update aem.js
    
    * Update columns.js
    
    * Update embed.js
    
    * Update embed.js
    
    * Update cards.css
    
    * Update embed.js
    
    * Update lite-vimeo-embed.js
    
    * Update blocks/embed/lite-vimeo-embed/lite-vimeo-embed.js
    
    Co-authored-by: Ryan Parrish <churchofslidin@gmail.com>
    
    * alternate
    
    * image for vimeo
    
    * fix
    
    * Update lite-vimeo-embed.js
    
    * Update blocks/embed/lite-vimeo-embed/lite-vimeo-embed.js
    
    Co-authored-by: Ryan Parrish <churchofslidin@gmail.com>
    
    * Update lite-vimeo-embed.js
    
    * Update lite-vimeo-embed.js
    
    * Update lite-vimeo-embed.js
    
    * Update decorate.js
    
    * Update embed.js
    
    * Update header.js
    
    ---------
    
    Co-authored-by: Ryan Parrish <churchofslidin@gmail.com>

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -33,15 +33,15 @@ function createRipple(event) {
 }
 
 function decorateMainMenu(section) {
-  const navHeaders = section.querySelectorAll('h3');
+  const navHeaders = section?.querySelectorAll('h3');
   if (!navHeaders) return;
   [...navHeaders].forEach((navHeader, i) => {
     const summaryTag = createTag('summary', { class: 'nav-summary' }, navHeader.textContent);
     const details = createTag('details', { class: `nav-detail detail-${i}` }, summaryTag);
     navHeader.replaceWith(details);
     const list = details.nextElementSibling;
-    const listLinks = list.querySelectorAll('li');
     if (!list) return;
+    const listLinks = list.querySelectorAll('li');
     details.append(list);
 
     /* toggle on mouseover in desktop */
