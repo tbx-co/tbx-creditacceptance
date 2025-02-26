1a079faa16ced81ac39b08a665b3d1a1517e5a29
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2025-02-26 13:28:33 -0500

    Accessibility: Icons Alt text  (#321)
    * Update helix-sitemap.yaml
    
    * Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -445,7 +445,7 @@ function decorateIcon(span, prefix = '', alt = '') {
   const img = document.createElement('img');
   img.dataset.iconName = iconName;
   img.src = `${window.hlx.codeBasePath}${prefix}/icons/${iconName}.svg`;
-  img.alt = alt;
+  img.alt = alt || iconName;
   img.loading = 'lazy';
   span.append(img);
 }
